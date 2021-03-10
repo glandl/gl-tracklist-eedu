04703bb7498c7d49840ba12b0d43075029c1b739
Gerald Landl <glandl@duality.at>
2021-03-10 10:31:43 +0100

    Auto-generated commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# Tracklist for eEducation Events
-Simple Tracklist read from table hosted on gdrive
--- src/app/shared/model/room.ts
+++
@@ -1,9 +0,0 @@
-export const roomAttributesMapping = {
-  Raum: 'Räume',
-  RaumLink: 'RaumLink',
-}
-
-export interface Room {
-  Raum: string;
-  RaumLink: string;
-}
