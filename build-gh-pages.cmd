b4f92a7e9f8b62e653edf7a2a93573543ca2f068
Gerald Landl <glandl@duality.at>
2021-03-10 22:32:15 +0100

    production server build statement

--- build-gh-pages.cmd
+++ build-gh-pages.cmd
@@ -1,2 +1,5 @@
 ng build --prod --base-href "https://glandl.github.io/gl-tracklist-eedu/"
 npx angular-cli-ghpages --dir=dist/gl-tracklist-eedu
+
+#eEducation Server
+ng build --prod --base-href "https://eeducation.at/tagungsprogramm"
