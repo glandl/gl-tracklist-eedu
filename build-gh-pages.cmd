5d9a6f5aecdcb170e1dee599a76af0d2ecd01f84
Gerald Landl <glandl@duality.at>
2021-03-13 17:00:21 +0100

    image handling
    - changed image url to relative
    - added flavicon
    - changed eeducation production path

--- build-gh-pages.cmd
+++ build-gh-pages.cmd
@@ -2,4 +2,4 @@ ng build --prod --base-href "https://glandl.github.io/gl-tracklist-eedu/"
 npx angular-cli-ghpages --dir=dist/gl-tracklist-eedu
 
 #eEducation Server
-ng build --prod --base-href "https://eeducation.at/tagungsprogramm"
+ng build --prod --base-href "https://eeducation.at/tagungsprogramm/"
