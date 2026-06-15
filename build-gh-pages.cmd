2acd3612a6251b42b1b43fb0244fafed9c04c06c
Gerald Landl <glandl@eurogym.info>
2026-06-15 18:21:54 +0200

    gh pages command changed

--- build-gh-pages.cmd
+++ build-gh-pages.cmd
@@ -1,5 +1,5 @@
 npx ng build --configuration=production --base-href "https://glandl.github.io/gl-tracklist-eedu/"
-npx angular-cli-ghpages --dir=dist/gl-tracklist-eedu/browser
+npx angular-cli-ghpages --dir=dist/gl-tracklist-eedu
 
 #eEducation Server
 npx ng build --configuration=production --base-href "https://eeducation.at/tagungsprogramm/"
